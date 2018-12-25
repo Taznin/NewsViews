87e6578bda01defea204ed5fbed4d0315bb9822f
Taznin <jahantaznin@yahoo.com>
2018-12-25 21:45:26 +0600

    google_signIn.

--- app/src/main/java/com/example/taznin/newsviews/LoginFragment.java
+++ app/src/main/java/com/example/taznin/newsviews/LoginFragment.java
@@ -1,6 +1,8 @@
 package com.example.taznin.newsviews;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,10 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
@@ -23,66 +28,62 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginFragment extends Fragment {
 
-    private SignInButton signInButton;
-    private final  static int RC_SIGN_IN=1;
-    private GoogleApiClient mGoogleSignInClient;
+    private SignInButton signIn;
+    private Button signOut;
+    public static final int RC_SIGN_IN = 1;
+    GoogleSignInClient mGoogleSignInClient;
+    public static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthListener;
-    private final static String TAG="INLogin";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.login_layout,container,false);
-        signInButton=(SignInButton) view.findViewById(R.id.btnSignIn);
+
+        signIn = (SignInButton)view.findViewById(R.id.btnSignIn);
+        signOut = (Button) view.findViewById(R.id.btnSignout);
         mAuth = FirebaseAuth.getInstance();
 
-        firebaseAuthListener= new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser() !=null){
-                    startActivity(new Intent(getActivity(),AccountActivity.class));
-                }
-            }
-        };
-
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient= new GoogleApiClient.Builder(getActivity().getApplicationContext())
-                .enableAutoManage(getActivity(), new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
 
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-                .build();
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 signIn();
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut(); //get signed out
+                signOut.setVisibility(View.GONE);
             }
         });
         return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(firebaseAuthListener);
-    }
+
 
     private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        signOut.setVisibility(View.VISIBLE);
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -110,13 +111,38 @@ public class LoginFragment extends Fragment {
                 .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Log.d(TAG, "authentication fail:");
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getActivity(),"you are not able to log in to google",Toast.LENGTH_LONG).show();
+                            updateUI(null);
                         }
+
+                        // ...
                     }
                 });
     }
-    public  void updateUI(){
+    public  void updateUI(FirebaseUser user){
+      //  signOut.setVisibility(View.VISIBLE);
 
+        signOut.setVisibility(View.VISIBLE);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            Toast.makeText(getActivity(), "Name of the user :" + personName + " user id is : " + personId, Toast.LENGTH_SHORT).show();
+
+        }
     }
 }

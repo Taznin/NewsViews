7f39a11d853b342ce020780746df4409d1065ade
Taznin <jahantaznin@yahoo.com>
2018-12-25 13:18:17 +0600

    exit_app

--- app/src/main/java/com/example/taznin/newsviews/MainActivity.java
+++ app/src/main/java/com/example/taznin/newsviews/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.taznin.newsviews;
 
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawer;
+    private NavigationView navigationView;
     private ActionBarDrawerToggle mToogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDrawer=(DrawerLayout) findViewById(R.id.myDrawer);
+        navigationView=findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
         mToogle= new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
         mDrawer.addDrawerListener(mToogle);
         mToogle.syncState();
@@ -26,5 +35,47 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.mItem_home:
+                Toast.makeText(this,"Home",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mItem_about:
+                Toast.makeText(this,"About",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mItem_exit:
+                exitApp();
+                //Toast.makeText(this,"Exit",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mItem_login:
+                Toast.makeText(this,"Login",Toast.LENGTH_LONG).show();
+                break;
+        }
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void exitApp(){
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Closing Activity")
+                .setMessage("Are you sure you want to close this activity?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        exitApp();
     }
 }
